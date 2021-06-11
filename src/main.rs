e39380c9013ba74e9e9f9de960f5ed9b9598bd7e
Helder S Ribeiro <git@agentofuser.com>
2021-06-11 09:17:19 -0400

    keyboard proxying works!!! \o/

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,20 @@
-use rdev::{grab, Event, EventType, Key};
+use rdev::{grab, simulate, Event, EventType, Key};
 
+fn grab_tab(event: Event) -> Option<Event> {
+    match event.event_type {
+        EventType::KeyPress(Key::Tab) => {
+            println!("Blocked a Tab!");
+            simulate(&EventType::KeyPress(Key::KeyK))
+                .expect("Failed to simulate");
+            println!("Replaced Tab with k!");
+            None
+        }
+        EventType::KeyRelease(Key::Tab) => None,
+        _ => Some(event),
+    }
+}
 fn main() {
     println!("Hello, world!");
+
+    grab(grab_tab).expect("Could not grab");
 }
